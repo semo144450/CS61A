3fe7c8d72b1b5e3cf1ec83a99a9b20b1de737a75
semo144450 <153276754+semo144450@users.noreply.github.com>
2025-01-06 15:18:31 +0800

    Create dis01.py

---
+++ Week02/dis01/dis01.py
@@ -0,0 +1,75 @@
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 'equal', 'not equal')
+    'not equal'
+    >>> if_function(3>2, 'bigger', 'smaller')
+    'bigger'
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+# if函数
+
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    61A
+    >>> print(result)
+    None
+    """
+    if cond():
+        return true_func()
+    else:
+        return false_func()
+
+
+def cond():
+    "*** YOUR CODE HERE ***"
+    return False
+
+
+def true_func():
+    "*** YOUR CODE HERE ***"
+    print("Welcome to")
+
+
+def false_func():
+    "*** YOUR CODE HERE ***"
+    print("61A")
+
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    Welcome to
+    61A
+    >>> print(result)
+    None
+    """
+    return if_function(cond(), true_func(), false_func())
+
+
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return False
+    k = 2
+    while k < n:
+        if n % k == 0:
+            return False
+        k += 1
+    return True
